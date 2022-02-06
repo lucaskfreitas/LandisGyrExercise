f733039ffa978251e0c7a60c3abc1907b880f90c
Lucas Kaminski <lucaskaminski@protonmail.com>
2022-02-06 15:59:48 -0300

    Refactoring the ConsoleInterface class

--- LandisGyrExercise/UserInterface/ConsoleInterface.cs
+++ LandisGyrExercise/UserInterface/ConsoleInterface.cs
@@ -21,7 +21,7 @@ namespace LandisGyrExercise.UserInterface
             {
                 try
                 {
-                    switch (QueryAction())
+                    switch (AskUserAction())
                     {
                         case UserAction.AddEndpoint:
                             _controller.AddEndpoint();
@@ -71,13 +71,9 @@ namespace LandisGyrExercise.UserInterface
 
         public void AskUserForEndpointData(Endpoint newEndpoint)
         {
-            newEndpoint.MeterModelId = AskUserForMeterModel();
-
+            newEndpoint.MeterModelId = AskUserForMeterModelId();
             newEndpoint.MeterNumber = AskUserForMeterNumber();
-
-            Console.WriteLine("What is the meter firmware version?");
-            newEndpoint.MeterFirmwareVersion = Console.ReadLine();
-
+            newEndpoint.MeterFirmwareVersion = AskUserForFirmwareVersion();
             newEndpoint.SwitchState = AskUserForSwitchState();
         }
 
@@ -89,38 +85,19 @@ namespace LandisGyrExercise.UserInterface
 
         public SwitchState AskUserForSwitchState()
         {
-            bool validInput = false;
-            int switchState = 0;
+            return AskUserFor<SwitchState>("Select a switch state: 0 = Disconnected, 1 = Connected, 2 = Armed");
+        }
 
-            while (!validInput)
-            {
-                Console.WriteLine("Select a switch state: 0 = Disconnected, 1 = Connected, 2 = Armed");
-
-                try
-                {
-                    if (!int.TryParse(Console.ReadLine(), out switchState))
-                        throw new ArgumentException();
-
-                    if (!Enum.IsDefined(typeof(SwitchState), switchState))
-                        throw new ArgumentException();
-
-                    validInput = true;
-                }
-                catch (ArgumentException)
-                {
-                    Console.WriteLine("Invalid option.");
-                }
-            }
-
-            return (SwitchState)switchState;
+        private static MeterModel AskUserForMeterModelId()
+        {
+            return AskUserFor<MeterModel>("What is the meter model ID?");
         }
 
         private static int AskUserForMeterNumber()
         {
-            bool validInput = false;
             int meterNumber = 0;
 
-            while (!validInput)
+            while (meterNumber <= 0)
             {
                 Console.WriteLine("What is the meter number?");
 
@@ -128,8 +105,6 @@ namespace LandisGyrExercise.UserInterface
                 {
                     if (!int.TryParse(Console.ReadLine(), out meterNumber))
                         throw new ArgumentException();
-
-                    validInput = true;
                 }
                 catch (ArgumentException)
                 {
@@ -140,37 +115,16 @@ namespace LandisGyrExercise.UserInterface
             return meterNumber;
         }
 
-        private static MeterModel AskUserForMeterModel()
+        private static string AskUserForFirmwareVersion()
         {
-            bool validInput = false;
-            int meterModel = 0;
-
-            while (!validInput)
-            {
-                Console.WriteLine("What is the meter model ID?");
-
-                try
-                {
-                    if (!int.TryParse(Console.ReadLine(), out meterModel))
-                        throw new ArgumentException();
-
-                    if (!Enum.IsDefined(typeof(MeterModel), meterModel))
-                        throw new ArgumentException();
-
-                    validInput = true;
-                }
-                catch (ArgumentException)
-                {
-                    Console.WriteLine("Invalid number.");
-                }
-            }
-
-            return (MeterModel)meterModel;
+            Console.WriteLine("What is the meter firmware version?");
+            return Console.ReadLine();
         }
 
-        private static UserAction QueryAction()
+        private static UserAction AskUserAction()
         {
-            StringBuilder stringBuilder = new("");
+            StringBuilder stringBuilder = new();
+            stringBuilder.AppendLine("");
             stringBuilder.AppendLine("What do you want to do?");
             stringBuilder.AppendLine("Possible actions:");
             stringBuilder.AppendLine("1 - Add a new endpoint");
@@ -178,22 +132,37 @@ namespace LandisGyrExercise.UserInterface
             stringBuilder.AppendLine("3 - Delete an endpoint");
             stringBuilder.AppendLine("4 - List all endpoint");
             stringBuilder.AppendLine("5 - Find an endpoint");
-            stringBuilder.AppendLine("6 - Exit");
-            stringBuilder.AppendLine("");
+            stringBuilder.Append("6 - Exit");
 
+            return AskUserFor<UserAction>(stringBuilder.ToString());
+        }
+
+        private static bool ConfirmAction()
+        {
+            Console.WriteLine("Are you sure? (y/n)");
+
+            return Console.ReadLine() switch
+            {
+                "y" or "Y" => true,
+                _ => false
+            };
+        }
+
+        private static T AskUserFor<T>(string message) where T : Enum
+        {
             bool validInput = false;
-            int userInput = 0;
+            int meterModel = 0;
 
             while (!validInput)
             {
-                Console.Write(stringBuilder.ToString());
+                Console.WriteLine(message);
 
                 try
                 {
-                    if (!int.TryParse(Console.ReadLine(), out userInput))
+                    if (!int.TryParse(Console.ReadLine(), out meterModel))
                         throw new ArgumentException();
 
-                    if (!Enum.IsDefined(typeof(UserAction), userInput))
+                    if (!Enum.IsDefined(typeof(T), meterModel))
                         throw new ArgumentException();
 
                     validInput = true;
@@ -204,19 +173,7 @@ namespace LandisGyrExercise.UserInterface
                 }
             }
 
-            return (UserAction)userInput;
-        }
-
-        private static bool ConfirmAction()
-        {
-            Console.WriteLine("Are you sure? (y/n)");
-
-            return Console.ReadLine() switch
-            {
-                "y" or "Y" => true,
-                "n" or "N" => false,
-                _ => throw new InvalidOperationException(),
-            };
+            return (T)Enum.Parse(typeof(T), meterModel.ToString());
         }
     }
 }
